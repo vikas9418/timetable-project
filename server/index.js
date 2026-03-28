baaf242ee8eadd2e39ca0b321d86cc2f343a332c
vikas9418 <vikassondarva53@gmail.com>
2026-03-28 17:44:20 +0530

    improved mongodb connection logic

--- server/index.js
+++ server/index.js
@@ -11,11 +11,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// 3. MongoDB Connection - Ab password '.env' file se aayega
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected Safely "))
-  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected Safely ✅");
+  } catch (err) {
+    console.error("MongoDB Connection Error: ❌", err.message);
+    process.exit(1); // अगर कनेक्ट न हो तो सर्वर बंद कर दे
+  }
+};
+
+connectDB(); // फंक्शन को कॉल करें
 
 // --- Baki sari APIs waisi hi rahengi ---
 
